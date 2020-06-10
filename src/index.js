d4f19dadd2620a145f37f2b8a1cb680bff00827b
Juliana Demarque <ju.dmrq@gmail.com>
2020-06-10 17:44:40 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import 'core-js/es/map'
+import 'core-js/es/set'
+import 'raf/polyfill'
+
+import App from '@analogica/components/App'
+
+const rootComponent = <App />
+const rootElement = document.getElementById('app')
+
+render(rootComponent, rootElement)
